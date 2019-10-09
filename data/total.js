a63f351684ade7dd2a282e4644c089f444c5a7c8
김세훈 <sehun5515@naver.com>
2019-10-09 14:48:02 +0000

    json파일 구성

---
+++ data/total.js
@@ -0,0 +1,60 @@
+var characterDataArr=[
+	{
+		"type":"lightType",
+		"name":"미호",
+		"img":"Miho",
+		"role":"Dealer",
+		"rank":"A",
+		"bodysize":[
+			"height":"154",
+			"weight":"38"
+		],
+		"stat":[
+			"LVL":1,
+			"HP":146,
+			"ATK":56,
+			"DEF":4,
+			"AGI":4.02,
+			"CRT":5,
+			"HIT":159,
+			"DOD":10
+		],
+		"cost":[
+			"parts":10,
+			"nutrition":16,
+			"electric":10
+		],
+		"skillpower":[
+			
+		],
+		"skills":[
+			{
+				"title":"askill1",
+				"type":"active",
+				"name":"바보 아냐?",
+				"img":"Miho",
+				"range":"4",
+				"description":"저격으로 피해(1.3배)를 줍니다.표적이나 도발 또는 표식 상태면 추가피해를 줍니다(30%).",
+				"AP":"6"
+				
+			},
+			{
+				"title":"askill2",
+				"type":"active",
+				"name":"마무리 저격",
+				"img":"Miho",
+				"range":"4",
+				"description":"저격으로 보호무시 피해를(1.6배) 줍니다. 대상의 HP가 낮을수록 피해량이 증가합니다.(+50%)"
+				"AP":"7"
+			},
+			{
+				"title":"pskill1",
+				"type":"passive",
+				"name":"전위 지원",
+				"img":"Miho",
+				"range":"0",
+				"description":"전투 개시 시, 자기 앞에 있는 보호기의 AP(+1)증가시키고 대상의 공격지원을 합니다."
+				"AP":"0"
+			}]
+	},
+];
