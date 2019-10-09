f46e46103db29156207078251525caf326f21e9e
김세훈 <sehun5515@naver.com>
2019-10-09 09:24:49 +0000

    Debug

--- js/randomImage.js
+++ js/randomImage.js
@@ -1,8 +1,8 @@
 var imgArray = new Array();
-imgArray[0] = "img/BGA/backgroundArt1.jpg";
-imgArray[1] = "img/BGA/backgroundArt2.jpg";
-imgArray[2] = "img/BGA/backgroundArt3.jpg";
-imgArray[3] = "img/BGA/backgroundArt4.jpg";
+imgArray[0] = "img/BGA/BGA1jpg";
+imgArray[1] = "img/BGA/BGA2.jpg";
+imgArray[2] = "img/BGA/BGA3.jpg";
+imgArray[3] = "img/BGA/BGA4.jpg";
 
 function showRandomImage(){
 	var imgNum = Math.round(Math.random()*3);
