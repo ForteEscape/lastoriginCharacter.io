49cae2210f8de2c9d4f670a640cb8431e3a6a2a1
김세훈 <sehun5515@naver.com>
2019-10-19 14:28:13 +0000

    #2 object  예시 추가

--- data/characterStat.js
+++ data/characterStat.js
@@ -1,28 +1,55 @@
 var characterDataArr=[
 	{
 		"type":"lightType",
-		"name":"T-14 미호",
-		"img":"Miho",
+		"name":"T-14 λ―ΈνΈ",
 		"role":"Dealer",
 		"rank":"A",
-		"bodysize":[
-			"height":"154",
-			"weight":"38"
-		],
-		"stat":[
-			"LVL":1,
-			"HP":146,
-			"ATK":56,
-			"DEF":4,
-			"AGI":4.02,
-			"CRT":5,
-			"HIT":159,
-			"DOD":10
-		],
+		"LVL":1,
+		"HP":146,
+		"ATK":56,
+		"DEF":4,
+		"AGI":4.02,
+		"CRT":5,
+		"HIT":159,
+		"DOD":10,
 		"cost":[
 			"parts":10,
 			"nutrition":16,
 			"electric":10
 		],
+		"skillLVL":[
+			1,
+			1,
+			1
+		],
+		"skillpower":[
+			
+		]
+	},
+	{
+		"type":"lightType",
+		"name":"T-14 λ―ΈνΈ",
+		"role":"Dealer",
+		"rank":"A",
+		"LVL":1,
+		"HP":146,
+		"ATK":56,
+		"DEF":4,
+		"AGI":4.02,
+		"CRT":5,
+		"HIT":159,
+		"DOD":10,
+		"cost":[
+			"parts":10,
+			"nutrition":16,
+			"electric":10
+		],
+		"skillLVL":[
+			
+		],
+		"skillpower":[
+			
+		]
 	}
+	
 ];
