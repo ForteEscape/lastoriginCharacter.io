2a9f1ab58ef50ecfe95079407eaa445cb04e5d59
김세훈 <sehun5515@naver.com>
2019-10-24 14:49:50 +0000

    #3 장비 페이지 부분 js오브젝트 파일 로드 완료

--- js/equipment.js
+++ js/equipment.js
@@ -19,6 +19,7 @@ function drawEquipmentPage(stat)
 	$('#name').append(equipmentName);
 	$('.image').append("<img src=\"img/equip/"+stat.img+".png\" style=\"width 100%\" />");
 	writeData('LVL', stat.LVL);
+	writeData('rank', stat.rank);
 	writeData('HP', stat.HP);
 	writeData('ATK', stat.ATK);
 	writeData('DEF', stat.DEF);
@@ -26,6 +27,7 @@ function drawEquipmentPage(stat)
 	writeData('CRT', stat.CRT);
 	writeData('HIT', stat.HIT);
 	writeData('DOD', stat.DOD);
+	writeData('descript', stat.descript);
 }
 
 function writeData(str1, str2)
@@ -36,6 +38,10 @@ function writeData(str1, str2)
 	}
 	else
 	{
-		$('#'+str1).append(' + '+str2+'%');
+		$('#'+str1).append(str2);
+	}
+	if(str1.endsWith('HIT') || str1.endsWith('CRT') || str1.endsWith('DOD') || str1.endsWith('ATK') || str1.endsWith('DEF') || str1.endsWith('AGI'))
+	{
+		$('#'+str1).append(' %');
 	}
 }
