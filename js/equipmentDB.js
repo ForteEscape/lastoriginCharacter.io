d0109d3e13415ed88775f50aaa0d46e137fbbd02
김세훈 <sehun5515@naver.com>
2019-10-27 14:56:05 +0000

    필터 재설계

--- js/equipmentDB.js
+++ js/equipmentDB.js
@@ -1,87 +1,273 @@
 function searchByEquipmentType(type_equipment)
 {	
+	//checked 상태가 되었는지 boolean 으로 체크한다.
+	var ischecked_chip = false;
+	var ischecked_os = false;
+	var ischecked_equipment = false;
+	var ischecked_designate = false;
+	
+	var chip_cnt = 0;
+	var os_cnt = 0;
+	var equipment_cnt = 0;
+	var designate_cnt = 0;
+	
 	if(type_equipment == chk_chip){
-		var $target = $('.chip');
+		chip_cnt++;
 		
-		if($target.css("display") == "none"){
-			$target.show();
+		if((chip_cnt%2) != 0){
+			ischecked_chip = true;
 		}
 		else{
-			$target.hide();
+			ischecked_chip = false;
+		}
+		
+		var $self = $('.chip');
+		var $target1 = $('.equipment');
+		var $target2 = $('.os');
+		var $target3 = $('.designate');
+		
+		if(ischecked_chip == true){
+			if(ischecked_os == true){
+				$target2.show();
+			}
+			else{
+				$target2.hide();
+			}
+		
+			if(ischecked_equipment == true){
+				$target1.show();
+			}
+			else{
+				$target2.hide();
+			}
+		
+			if(ischecked_designate == true){
+				$target3.show();
+			}
+			else{
+				$target3.hide();
+			}
+		}
+		else if(ischecked_chip == false){
+			$self.hide();
 		}
 	}
 	else if(type_equipment == chk_os){
-		var $target = $('.os');
+		os_cnt++;
 		
-		if($target.css("display") == "none"){
-			$target.show();
+		if((os_cnt%2) != 0){
+			ischecked_os = true;
 		}
 		else{
-			$target.hide();
+			ischecked_os = false;
+		}
+		
+		var $self = $('.os');
+		var $target1 = $('.equipment');
+		var $target2 = $('.chip');
+		var $target3 = $('.designate');
+		
+		if(ischecked_os == true){
+			if(ischecked_chip == true){
+				$target2.show();
+			}
+			else{
+				$target2.hide();
+			}
+		
+			if(ischecked_equipment == true){
+				$target1.show();
+			}
+			else{
+				$target2.hide();
+			}
+		
+			if(ischecked_designate == true){
+				$target3.show();
+			}
+			else{
+				$target3.hide();
+			}
+		}
+		else if(ischecked_os == false){
+			$self.hide();
 		}
 	}
 	else if(type_equipment == chk_equ){
-		var $target = $('.equipment');
+		equipment_cnt++;
 		
-		if($target.css("display") == "none"){
-			$target.show();
+		if((equipment_cnt%2) != 0){
+			ischecked_equipment = true;
 		}
 		else{
-			$target.hide();
+			ischecked_equipment = false;
+		}
+		
+		var $self = $('.equipment');
+		var $target1 = $('.chip');
+		var $target2 = $('.os');
+		var $target3 = $('.designate');
+		
+		if(ischecked_equipment == true){
+			if(ischecked_os == true){
+				$target2.show();
+			}
+			else{
+				$target2.hide();
+			}
+		
+			if(ischecked_chip == true){
+				$target1.show();
+			}
+			else{
+				$target2.hide();
+			}
+		
+			if(ischecked_designate == true){
+				$target3.show();
+			}
+			else{
+				$target3.hide();
+			}
+		}
+		else if(ischecked_equipment == false){
+			$self.hide();
 		}
 	}
-	else if(type_equipment == chk_dequ){
-		var $target = $('.designate');
+	/*else if(type_equipment == chk_dequ){
+		ischecked_designate = true;
 		
-		if($target.css("display") == "none"){
-			$target.show();
+		var $target1 = $('.equipment');
+		var $target2 = $('.os');
+		var $target3 = $('.chip');
+		
+		if($target1.css("display") == "none"){
+			$target1.show();
 		}
 		else{
-			$target.hide();
+			$target1.hide();
 		}
-	}
+		
+		if($target2.css("display") == "none"){
+			$target2.show();
+		}
+		else{
+			$target2.hide();
+		}
+		
+		if($target3.css("display") == "none"){
+			$target3.show();
+		}
+		else{
+			$target3.hide();
+		}
+	}*/
 }
 
-function searchByEquipmentRank(rank_equipment)
+/*function searchByEquipmentRank(rank_equipment)
 {
 	if(rank_equipment == SS){
-		var $target = $('.SS');
+		var $target1 = $('.B');
+		var $target2 = $('.A');
+		var $target3 = $('.S');
 		
-		if($target.css("display") == "none"){
-			$target.show();
+		if($target1.css("display") == "none"){
+			$target1.show();
 		}
 		else{
-			$target.hide();
+			$target1.hide();
+		}
+		
+		if($target2.css("display") == "none"){
+			$target2.show();
+		}
+		else{
+			$target2.hide();
+		}
+		
+		if($target3.css("display") == "none"){
+			$target3.show();
+		}
+		else{
+			$target3.hide();
 		}
 	}
 	else if(rank_equipment == S){
-		var $target = $('.S');
+		var $target1 = $('.B');
+		var $target2 = $('.A');
+		var $target3 = $('.SS');
 		
-		if($target.css("display") == "none"){
-			$target.show();
+		if($target1.css("display") == "none"){
+			$target1.show();
 		}
 		else{
-			$target.hide();
+			$target1.hide();
+		}
+		
+		if($target2.css("display") == "none"){
+			$target2.show();
+		}
+		else{
+			$target2.hide();
+		}
+		
+		if($target3.css("display") == "none"){
+			$target3.show();
+		}
+		else{
+			$target3.hide();
 		}
 	}
 	else if(rank_equipment == A){
-		var $target = $('.A');
+		var $target1 = $('.B');
+		var $target2 = $('.SS');
+		var $target3 = $('.S');
 		
-		if($target.css("display") == "none"){
-			$target.show();
+		if($target1.css("display") == "none"){
+			$target1.show();
 		}
 		else{
-			$target.hide();
+			$target1.hide();
+		}
+		
+		if($target2.css("display") == "none"){
+			$target2.show();
+		}
+		else{
+			$target2.hide();
+		}
+		
+		if($target3.css("display") == "none"){
+			$target3.show();
+		}
+		else{
+			$target3.hide();
 		}
 	}
 	else if(rank_equipment == B){
-		var $target = $('.B');
+		var $target1 = $('.SS');
+		var $target2 = $('.A');
+		var $target3 = $('.S');
 		
-		if($target.css("display") == "none"){
-			$target.show();
+		if($target1.css("display") == "none"){
+			$target1.show();
 		}
 		else{
-			$target.hide();
+			$target1.hide();
+		}
+		
+		if($target2.css("display") == "none"){
+			$target2.show();
+		}
+		else{
+			$target2.hide();
+		}
+		
+		if($target3.css("display") == "none"){
+			$target3.show();
+		}
+		else{
+			$target3.hide();
 		}
 	}
-}
+}*/
