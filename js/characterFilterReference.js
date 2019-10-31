f275454c9cca70a50b55cf56bfd48199723c8c73
김세훈 <sehun5515@naver.com>
2019-10-31 14:52:20 +0000

    #4 characterFilter 구현 시작

---
+++ js/characterFilterReference.js
@@ -0,0 +1,160 @@
+//base variables
+var $bioroid = $('.bioroid');
+var $ags = $('.ags');
+	
+var $rankB = $('.B');
+var $rankA = $('.A');
+var $rankS = $('.S');
+var $rankSS = $('.SS');
+	
+var $lighttype = $('.light');
+var $mobiletype = $('.mobile');
+var $heavytype = $('.heavy');
+	
+var $attacker = $('.attacker');
+var $tanker = $('.tanker');
+var $supporter = $('.supporter');
+	
+var $injured = $('.injured');
+var $live2D = $('.live2D');
+	
+	//combination variables
+var $bioroid_B = $('.bioroid.B');
+	
+var $bioroid_B_lighttype = $('.bioroid.B.light');
+var $bioroid_B_lighttype_attacker = $('.bioroid.B.light.attacker');
+var $bioroid_B_lighttype_attacker_injured = $('.bioroid.B.light.attacker.injured');
+var $bioroid_B_lighttype_attacker_live2D = $('.bioroid.B.light.attacker.live2D');
+var $bioroid_B_lighttype_tanker = $('.bioroid.B.light.tanker');
+var $bioroid_B_lighttype_tanker_injured = $('.bioroid.B.light.tanker.injured');
+var $bioroid_B_lighttype_tanker_live2D = $('.bioroid.B.light.tanker.live2D');
+var $bioroid_B_lighttype_supporter = $('.bioroid.B.light.supporter');
+var $bioroid_B_lighttype_supporter_injured = $('.bioroid.B.light.supporter.injured');
+var $bioroid_B_lighttype_supporter_live2D = $('.bioroid.B.light.supporter.live2D');
+	
+var $bioroid_B_mobiletype = $('.bioroid.B.mobile');
+var $bioroid_B_mobiletype_attacker = $('.bioroid.B.mobile.attacker')
+var $bioroid_B_mobiletype_attacker_injured = $('.bioroid.B.mobile.attacker.injured');
+var $bioroid_B_mobiletype_attacker_live2D = $('.bioroid.B.mobile.attacker.live2D');
+var $bioroid_B_mobiletype_tanker = $('.bioroid.B.mobile.tanker');
+var $bioroid_B_mobiletype_tanker_injured = $('.bioroid.B.mobile.tanker.injured');
+var $bioroid_B_mobiletype_tanker_live2D = $('.bioroid.B.mobile.tanker.live2D');
+var $bioroid_B_mobiletype_supporter = $('.bioroid.B.mobile.supporter');
+var $bioroid_B_mobiletype_supporter_injured = $('.bioroid.B.mobile.supporter.injured');
+var $bioroid_B_mobiletype_supporter_live2D = $('.bioroid.B.mobile.supporter.live2D');
+	
+var $bioroid_B_heavytype = $('.bioroid.B.heavy');
+var $bioroid_B_heavytype_attacker = $('.bioroid.B.heavy.attacker');
+var $bioroid_B_heavytype_attacker_injured = $('.bioroid.B.heavy.attacker.injured');
+var $bioroid_B_heavytype_attacker_live2D = $('.bioroid.B.heavy.attacker.live2D');
+var $bioroid_B_heavytype_tanker = $('.bioroid.B.heavy.tanker');
+var $bioroid_B_heavytype_tanker_injured = $('.bioroid.B.heavy.tanker.injured');
+var $bioroid_B_heavytype_tanker_live2D = $('.bioroid.B.heavy.tanker.live2D');
+var $bioroid_B_heavytype_supporter = $('.bioroid.B.heavy.supporter');
+var $bioroid_B_heavytype_supporter_injured = $('.bioroid.B.heavy.supporter.injured');
+var $bioroid_B_heavytype_supporter_live2D = $('.bioroid.B.heavy.supporter.live2D');
+//======================================== filter variable of rank B ==============================================
+var $bioroid_A = $('.bioroid.A');
+	
+var $bioroid_A_lighttype = $('.bioroid.A.light');
+var $bioroid_A_lighttype_attacker = $('.bioroid.A.light.attacker');
+var $bioroid_A_lighttype_attacker_injured = $('.bioroid.A.light.attacker.injured');
+var $bioroid_A_lighttype_attacker_live2D = $('.bioroid.A.light.attacker.live2D');
+var $bioroid_A_lighttype_tanker = $('.bioroid.A.light.tanker');
+var $bioroid_A_lighttype_tanker_injured = $('.bioroid.A.light.tanker.injured');
+var $bioroid_A_lighttype_tanker_live2D = $('.bioroid.A.light.tanker.live2D');
+var $bioroid_A_lighttype_supporter = $('.bioroid.A.light.supporter');
+var $bioroid_A_lighttype_supporter_injured = $('.bioroid.A.light.supporter.injured');
+var $bioroid_A_lighttype_supporter_live2D = $('.bioroid.A.light.supporter.live2D');
+	
+var $bioroid_A_mobiletype = $('.bioroid.A.mobile');
+var $bioroid_A_mobiletype_attacker = $('.bioroid.A.mobile.attacker')
+var $bioroid_A_mobiletype_attacker_injured = $('.bioroid.A.mobile.attacker.injured');
+var $bioroid_A_mobiletype_attacker_live2D = $('.bioroid.A.mobile.attacker.live2D');
+var $bioroid_A_mobiletype_tanker = $('.bioroid.A.mobile.tanker');
+var $bioroid_A_mobiletype_tanker_injured = $('.bioroid.A.mobile.tanker.injured');
+var $bioroid_A_mobiletype_tanker_live2D = $('.bioroid.A.mobile.tanker.live2D');
+var $bioroid_A_mobiletype_supporter = $('.bioroid.A.mobile.supporter');
+var $bioroid_A_mobiletype_supporter_injured = $('.bioroid.A.mobile.supporter.injured');
+var $bioroid_A_mobiletype_supporter_live2D = $('.bioroid.A.mobile.supporter.live2D');
+	
+var $bioroid_A_heavytype = $('.bioroid.A.heavy');
+var $bioroid_A_heavytype_attacker = $('.bioroid.A.heavy.attacker');
+var $bioroid_A_heavytype_attacker_injured = $('.bioroid.A.heavy.attacker.injured');
+var $bioroid_A_heavytype_attacker_live2D = $('.bioroid.A.heavy.attacker.live2D');
+var $bioroid_A_heavytype_tanker = $('.bioroid.A.heavy.tanker');
+var $bioroid_A_heavytype_tanker_injured = $('.bioroid.A.heavy.tanker.injured');
+var $bioroid_A_heavytype_tanker_live2D = $('.bioroid.A.heavy.tanker.live2D');
+var $bioroid_A_heavytype_supporter = $('.bioroid.A.heavy.supporter');
+var $bioroid_A_heavytype_supporter_injured = $('.bioroid.A.heavy.supporter.injured');
+var $bioroid_A_heavytype_supporter_live2D = $('.bioroid.A.heavy.supporter.live2D');
+//======================================== filter variable of rank A ==============================================
+var $bioroid_S = $('.bioroid.S');
+	
+var $bioroid_S_lighttype = $('.bioroid.S.light');
+var $bioroid_S_lighttype_attacker = $('.bioroid.S.light.attacker');
+var $bioroid_S_lighttype_attacker_injured = $('.bioroid.S.light.attacker.injured');
+var $bioroid_S_lighttype_attacker_live2D = $('.bioroid.S.light.attacker.live2D');
+var $bioroid_S_lighttype_tanker = $('.bioroid.S.light.tanker');
+var $bioroid_S_lighttype_tanker_injured = $('.bioroid.S.light.tanker.injured');
+var $bioroid_S_lighttype_tanker_live2D = $('.bioroid.S.light.tanker.live2D');
+var $bioroid_S_lighttype_supporter = $('.bioroid.S.light.supporter');
+var $bioroid_S_lighttype_supporter_injured = $('.bioroid.S.light.supporter.injured');
+var $bioroid_S_lighttype_supporter_live2D = $('.bioroid.S.light.supporter.live2D');
+	
+var $bioroid_S_mobiletype = $('.bioroid.S.mobile');
+var $bioroid_S_mobiletype_attacker = $('.bioroid.S.mobile.attacker')
+var $bioroid_S_mobiletype_attacker_injured = $('.bioroid.S.mobile.attacker.injured');
+var $bioroid_S_mobiletype_attacker_live2D = $('.bioroid.S.mobile.attacker.live2D');
+var $bioroid_S_mobiletype_tanker = $('.bioroid.S.mobile.tanker');
+var $bioroid_S_mobiletype_tanker_injured = $('.bioroid.S.mobile.tanker.injured');
+var $bioroid_S_mobiletype_tanker_live2D = $('.bioroid.S.mobile.tanker.live2D');
+var $bioroid_S_mobiletype_supporter = $('.bioroid.S.mobile.supporter');
+var $bioroid_S_mobiletype_supporter_injured = $('.bioroid.S.mobile.supporter.injured');
+var $bioroid_S_mobiletype_supporter_live2D = $('.bioroid.S.mobile.supporter.live2D');
+	
+var $bioroid_S_heavytype = $('.bioroid.S.heavy');
+var $bioroid_S_heavytype_attacker = $('.bioroid.S.heavy.attacker');
+var $bioroid_S_heavytype_attacker_injured = $('.bioroid.S.heavy.attacker.injured');
+var $bioroid_S_heavytype_attacker_live2D = $('.bioroid.S.heavy.attacker.live2D');
+var $bioroid_S_heavytype_tanker = $('.bioroid.S.heavy.tanker');
+var $bioroid_S_heavytype_tanker_injured = $('.bioroid.S.heavy.tanker.injured');
+var $bioroid_S_heavytype_tanker_live2D = $('.bioroid.S.heavy.tanker.live2D');
+var $bioroid_S_heavytype_supporter = $('.bioroid.S.heavy.supporter');
+var $bioroid_S_heavytype_supporter_injured = $('.bioroid.S.heavy.supporter.injured');
+var $bioroid_S_heavytype_supporter_live2D = $('.bioroid.S.heavy.supporter.live2D');
+//======================================== filter variable of rank S ==============================================
+var $bioroid_SS = $('.bioroid.SS');
+var $bioroid_SS_lighttype = $('.bioroid.SS.light');
+var $bioroid_SS_lighttype_attacker = $('.bioroid.SS.light.attacker');
+var $bioroid_SS_lighttype_attacker_injured = $('.bioroid.SS.light.attacker.injured');
+var $bioroid_SS_lighttype_attacker_live2D = $('.bioroid.SS.light.attacker.live2D');
+var $bioroid_SS_lighttype_tanker = $('.bioroid.SS.light.tanker');
+var $bioroid_SS_lighttype_tanker_injured = $('.bioroid.SS.light.tanker.injured');
+var $bioroid_SS_lighttype_tanker_live2D = $('.bioroid.SS.light.tanker.live2D');
+var $bioroid_SS_lighttype_supporter = $('.bioroid.SS.light.supporter');
+var $bioroid_SS_lighttype_supporter_injured = $('.bioroid.SS.light.supporter.injured');
+var $bioroid_SS_lighttype_supporter_live2D = $('.bioroid.SS.light.supporter.live2D');
+	
+var $bioroid_SS_mobiletype = $('.bioroid.SS.mobile');
+var $bioroid_SS_mobiletype_attacker = $('.bioroid.SS.mobile.attacker')
+var $bioroid_SS_mobiletype_attacker_injured = $('.bioroid.SS.mobile.attacker.injured');
+var $bioroid_SS_mobiletype_attacker_live2D = $('.bioroid.SS.mobile.attacker.live2D');
+var $bioroid_SS_mobiletype_tanker = $('.bioroid.SS.mobile.tanker');
+var $bioroid_SS_mobiletype_tanker_injured = $('.bioroid.SS.mobile.tanker.injured');
+var $bioroid_SS_mobiletype_tanker_live2D = $('.bioroid.SS.mobile.tanker.live2D');
+var $bioroid_SS_mobiletype_supporter = $('.bioroid.SS.mobile.supporter');
+var $bioroid_SS_mobiletype_supporter_injured = $('.bioroid.SS.mobile.supporter.injured');
+var $bioroid_SS_mobiletype_supporter_live2D = $('.bioroid.SS.mobile.supporter.live2D');
+	
+var $bioroid_SS_heavytype = $('.bioroid.SS.heavy');
+var $bioroid_SS_heavytype_attacker = $('.bioroid.SS.heavy.attacker');
+var $bioroid_SS_heavytype_attacker_injured = $('.bioroid.SS.heavy.attacker.injured');
+var $bioroid_SS_heavytype_attacker_live2D = $('.bioroid.SS.heavy.attacker.live2D');
+var $bioroid_SS_heavytype_tanker = $('.bioroid.SS.heavy.tanker');
+var $bioroid_SS_heavytype_tanker_injured = $('.bioroid.SS.heavy.tanker.injured');
+var $bioroid_SS_heavytype_tanker_live2D = $('.bioroid.SS.heavy.tanker.live2D');
+var $bioroid_SS_heavytype_supporter = $('.bioroid.SS.heavy.supporter');
+var $bioroid_SS_heavytype_supporter_injured = $('.bioroid.SS.heavy.supporter.injured');
+var $bioroid_SS_heavytype_supporter_live2D = $('.bioroid.SS.heavy.supporter.live2D');
+//======================================== filter variable of rank SS ==============================================
