659fcc5e7b906bcd5e33b525d25be40b07c92bde
김세훈 <sehun5515@naver.com>
2019-11-07 13:48:02 +0000

    add waveform in BGM

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -11,27 +11,52 @@ TrackList=[
 	},
 	{
 		url: './BGM/BGM_Battle_01.mp3',
-		title: 'LastOrigin battle music1',
+		title: 'LastOrigin battle music 1',
 		year: '2019'
 	},
 	{
 		url: './BGM/BGM_Battle_02.mp3',
-		title: 'LastOrigin battle music2',
+		title: 'LastOrigin battle music 2',
 		year: '2019'
 	},
 	{
 		url: './BGM/BGM_Battle_03.mp3',
-		title: 'LastOrigin battle music3',
+		title: 'LastOrigin battle music 3',
 		year: '2019'
 	},
 	{
 		url: './BGM/BGM_Battle_Boss_01.mp3',
-		title: 'LastOrigin boss battle music1',
+		title: 'LastOrigin boss battle music 1',
 		year: '2019'
 	},
 	{
 		url: './BGM/BGM_Battle_Boss_02.mp3',
-		title: 'LastOrigin boss battle music2',
+		title: 'LastOrigin boss battle music 2',
+		year: '2019'
+	},
+	{
+		url: './BGM/BGM_Talk_01.mp3',
+		title: 'LastOrigin ingame conversation music 1',
+		year: '2019'
+	},
+	{
+		url: './BGM/BGM_Talk_02.mp3',
+		title: 'LastOrigin ingame conversation music 2',
+		year: '2019'
+	},
+	{
+		url: './BGM/BGM_Talk_03.mp3',
+		title: 'LastOrigin ingame conversation music 3',
+		year: '2019'
+	},
+	{
+		url: './BGM/BGM_Talk_04.mp3',
+		title: 'LastOrigin ingame conversation music 4',
+		year: '2019'
+	},
+	{
+		url: './BGM/BGM_Talk_05.mp3',
+		title: 'LastOrigin ingame conversation music 5',
 		year: '2019'
 	}
 ];
