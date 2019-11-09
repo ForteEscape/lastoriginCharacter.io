44bc02d020e841f12228e4f57b3bddb9a9c14db9
김세훈 <sehun5515@naver.com>
2019-11-09 07:35:53 +0000

    #7 implement goal 2 in issue #7

--- js/calc_equipmentEXP.js
+++ js/calc_equipmentEXP.js
@@ -1,3 +1,5 @@
+var func_call_cnt = 0;
+
 window.onload = async function(){
 	var equipment_data = await loadEquipmentStatDataArr()
 	
@@ -23,7 +25,6 @@ function calcEquipmentData(data){
 }
 
 async function equipmentChk(param){
-	var func_call_cnt = 0;
 	var equipment_arr = await loadEquipmentStatDataArr();
 	var equipment_stat = await loadEquipmentStatData(equipment_arr[param].id);
 	
@@ -35,8 +36,40 @@ async function equipmentChk(param){
 
 function drawEquipmentPage(stat, fcnt)
 {
+	console.log(fcnt);
+	
 	if(fcnt > 1){
+		var statTextVal = $('.spec-item[name=stat]');
+		var nameTextVal = $('.equipmentName');
+		var levelTextVal = $('.equipmentLv');
+		var descTextVal = $('#descript');
 		
+		statTextVal.text(" ");
+		nameTextVal.text(" ");
+		levelTextVal.text(" ");
+		descTextVal.text(" ");
+		$('#img *').remove();
+		
+		if(stat.nickname){
+			var equipmentName = stat.nickname;
+		}
+		else{
+			var equipmentName = stat.name;
+		}
+		document.title = equipmentName+'정보';
+	
+		$('#name').append(equipmentName);
+		$('.image').append("<img src=\"img/equip/"+stat.img+".png\" style=\"width 100%\" />");
+		writeData('LVL', stat.LVL);
+		writeData('rank', stat.rank);
+		writeData('HP', stat.HP);
+		writeData('ATK', stat.ATK);
+		writeData('DEF', stat.DEF);
+		writeData('AGI', stat.AGI);
+		writeData('CRT', stat.CRT);
+		writeData('HIT', stat.HIT);
+		writeData('DOD', stat.DOD);
+		writeData('descript', stat.descript);
 	}
 	else{
 		if(stat.nickname){
@@ -76,6 +109,12 @@ function writeData(str1, str2)
 	if(str1.endsWith('HIT') || str1.endsWith('CRT') || str1.endsWith('DOD') || str1.endsWith('ATK') || str1.endsWith('DEF') || str1.endsWith('AGI'))
 	{
 		$('#'+str1).append(' %');
+	}
+	if(str1.endsWith('HP')){
+		$('#'+str1).prepend('추가 HP: +');
+	}
+	if(str1.endsWith('LVL')){
+		$('#'+str1).prepend('LV.');
 	}
 }
 
