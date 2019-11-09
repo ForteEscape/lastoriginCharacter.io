39c9c9bb18cc86828c3f7481ace42b4265dd7879
김세훈 <sehun5515@naver.com>
2019-11-09 14:39:56 +0000

    #7 redesign calc_equipmentEXP
    design reference in equipment calculator worksheet
    need some redesign function relation

--- js/calc_equipmentEXP.js
+++ js/calc_equipmentEXP.js
@@ -1,10 +1,9 @@
-var func_call_cnt = 0;
+var eq_level = 1;
 
 window.onload = async function(){
 	var equipment_data = await loadEquipmentStatDataArr()
 	
 	drawEquipmentList(equipment_data);
-	calcEquipmentData(equipment_data);
 }
 
 function drawEquipmentList(data){
@@ -17,115 +16,45 @@ async function equipmentChk(param){
 	var equipment_arr = await loadEquipmentStatDataArr();
 	var equipment_stat = await loadEquipmentStatData(equipment_arr[param].id);
 	
-	func_call_cnt++;
+	//console.log($(this).)
 	
-	drawEquipmentPage(equipment_stat, func_call_cnt);
-	
+	calcEquipmentData(equipment_stat);
 }
 
-function drawEquipmentPage(stat, fcnt)
-{
-	console.log(fcnt);
-	
-	if(fcnt > 1){
-		var statTextVal = $('.spec-item[name=stat]');
-		var nameTextVal = $('.equipmentName');
-		var levelTextVal = $('.equipmentLv');
-		var descTextVal = $('#descript');
-		
-		statTextVal.text(" ");
-		nameTextVal.text(" ");
-		levelTextVal.text(" ");
-		descTextVal.text(" ");
-		$('#img *').remove();
-		
-		if(stat.nickname){
-			var equipmentName = stat.nickname;
-		}
-		else{
-			var equipmentName = stat.name;
-		}
-		document.title = equipmentName+'정보';
-	
-		$('#name').append(equipmentName);
-		$('.image').append("<img src=\"img/equip/"+stat.img+".png\" style=\"width 100%\" />");
-		writeData('LVL', stat.LVL);
-		writeData('rank', stat.rank);
-		writeData('HP', stat.HP);
-		writeData('ATK', stat.ATK);
-		writeData('DEF', stat.DEF);
-		writeData('AGI', stat.AGI);
-		writeData('CRT', stat.CRT);
-		writeData('HIT', stat.HIT);
-		writeData('DOD', stat.DOD);
-		writeData('descript', stat.descript);
+function setEquipmentLevel(lv){
+	if(lv == 0){
+		$('#eqlevel').text("Lv");
 	}
 	else{
-		if(stat.nickname){
-			var equipmentName = stat.nickname;
-		}
-		else{
-			var equipmentName = stat.name;
-		}
-		document.title = equipmentName+'정보';
-	
-		$('#name').append(equipmentName);
-		$('.image').append("<img src=\"img/equip/"+stat.img+".png\" style=\"width 100%\" />");
-		writeData('LVL', stat.LVL);
-		writeData('rank', stat.rank);
-		writeData('HP', stat.HP);
-		writeData('ATK', stat.ATK);
-		writeData('DEF', stat.DEF);
-		writeData('AGI', stat.AGI);
-		writeData('CRT', stat.CRT);
-		writeData('HIT', stat.HIT);
-		writeData('DOD', stat.DOD);
-		writeData('descript', stat.descript);
+		eq_level = lv;
+		$('#eqlevel').text("Lv. "+lv);
 	}
+	
 	
 }
 
-function writeData(str1, str2)
-{
-	if(str2 == -1)
-	{
-		$('#'+str1).append('???');
-	}
-	else
-	{
-		$('#'+str1).append(str2);
-	}
-	if(str1.endsWith('HIT') || str1.endsWith('CRT') || str1.endsWith('DOD') || str1.endsWith('ATK') || str1.endsWith('DEF') || str1.endsWith('AGI'))
-	{
-		$('#'+str1).append(' %');
-	}
-	if(str1.endsWith('HP')){
-		$('#'+str1).prepend('추가 HP: +');
-	}
-	if(str1.endsWith('LVL')){
-		$('#'+str1).prepend('LV.');
-	}
-}
-
-function getEquipmentLevel(){
-	if(!$('#eqLevel').text()){
-		console.log("empty");
-	}
-	else console.log($('#eqLevel').text());
-	
-	//return $(this).text();
-	
-	
+function init_equipment(){
+	$('#equipment_name').text("장비 목록");
 }
 
 function calcEquipmentData(data){
-	var equipment_HP = data.HP;
-	var equipment_ATK = data.ATK;
-	var equipment_DEF = data.DEF;
-	var equipment_AGI = data.AGI;
-	var equipment_DOD = data.DOD;
-	var equipment_CRT = data.CRT;
-	var equipemnt_HIT = data.HIT;
+	$('#equipment_name').text(data.name);
+	$('#rank_slot1').text(data.rank);
+	$('#currentLv_slot1').text(data.LVL);
+	$('#currentEXP_slot1').text(data.curEXP);
 	
+	var table_SS = [1500, 2325, 3150, 3975, 4800, 5625, 6450, 7275, 8100, 8925];
+	var occurEXP = 0;
+	
+	if(eq_level < data.LVL){
+		alert('목표 레벨은 현재 레벨보다 낮을 수 없습니다!');
+	}
+	else{
+		for(var i = data.LVL; i<(eq_level-data.LVL); i++){
+			occurEXP = occurEXP+table_SS[i];
+			console.log(occurEXP);
+		}
+		$('.occuredEXP_slot1').text(occurEXP);
+	}
 }
 
