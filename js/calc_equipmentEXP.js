c4404c6b03e046ac84e8e555c63ee7c55e9d0d67
김세훈 <sehun5515@naver.com>
2019-11-10 14:55:27 +0000

    #7 remodeled calculator javascript file
    modify relation of function

--- js/calc_equipmentEXP.js
+++ js/calc_equipmentEXP.js
@@ -1,4 +1,4 @@
-var eq_level = 1;
+var data_param;
 
 window.onload = async function(){
 	var equipment_data = await loadEquipmentStatDataArr()
@@ -12,49 +12,24 @@ function drawEquipmentList(data){
 	}
 }
 
-async function equipmentChk(param){
-	var equipment_arr = await loadEquipmentStatDataArr();
-	var equipment_stat = await loadEquipmentStatData(equipment_arr[param].id);
-	
-	//console.log($(this).)
-	
-	calcEquipmentData(equipment_stat);
-}
-
-function setEquipmentLevel(lv){
-	if(lv == 0){
-		$('#eqlevel').text("Lv");
-	}
-	else{
-		eq_level = lv;
-		$('#eqlevel').text("Lv. "+lv);
-	}
-	
-	
-}
-
 function init_equipment(){
 	$('#equipment_name').text("장비 목록");
 }
 
-function calcEquipmentData(data){
-	$('#equipment_name').text(data.name);
-	$('#rank_slot1').text(data.rank);
-	$('#currentLv_slot1').text(data.LVL);
-	$('#currentEXP_slot1').text(data.curEXP);
+async function equipmentChk(param){
+	var equipment_arr = await loadEquipmentStatDataArr();
+	var equipment_stat = await loadEquipmentStatData(equipment_arr[param].id);
 	
-	var table_SS = [1500, 2325, 3150, 3975, 4800, 5625, 6450, 7275, 8100, 8925];
-	var occurEXP = 0;
+	data_param = param;
+	return equipment_stat;
+}
+
+function calcEXP(){
+	var equipmentParam = data_param;
+	var equipment_stat = equipmentChk(equipmentParam);
+	var equipment_goal_level = $('#level option:selected').attr('id');
 	
-	if(eq_level < data.LVL){
-		alert('목표 레벨은 현재 레벨보다 낮을 수 없습니다!');
-	}
-	else{
-		for(var i = data.LVL; i<(eq_level-data.LVL); i++){
-			occurEXP = occurEXP+table_SS[i];
-			console.log(occurEXP);
-		}
-		$('.occuredEXP_slot1').text(occurEXP);
-	}
+	console.log(equipment_stat.LVL);
+	console.log(equipment_goal_level);
 }
 
